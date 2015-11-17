ad72fec64828e8047f99114b28958e4179cb0461
Stepan Vardanyan <styopdev@hotmail.com>
2015-11-17 12:55:24 +0400

    added fb strategy

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var passport = require('passport');
-var Strategy = require('passport-twitter').Strategy;
-
+var twitterStrategy = require('passport-twitter').Strategy;
+var facebookStrategy = require('passport-facebook').Strategy;
+var googleStrategy = require('passport-google-plus');
 
 // Configure the Twitter strategy for use by Passport.
 //
@@ -11,19 +12,32 @@ var Strategy = require('passport-twitter').Strategy;
 // with a user object, which will be set at `req.user` in route handlers after
 // authentication.
 
-passport.use(new Strategy({
-    consumerKey: "5QUoJc5SW5P5flcf7SA28PS0m",
-    consumerSecret: "oa2V4gmlGlhJVih6ZMDEPl0Cu0syJZJvNCqXYWPA2UGfTHcZtw",
-    callbackURL: 'http://oauthall.herokuapp.com/login/twitter/return'
-  },
-  function(token, tokenSecret, profile, cb) {
-    // In this example, the user's Twitter profile is supplied as the user
-    // record.  In a production-quality application, the Twitter profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
-    return cb(null, profile);
-  }));
+passport.use(new twitterStrategy({
+        consumerKey: "5QUoJc5SW5P5flcf7SA28PS0m",
+        consumerSecret: "oa2V4gmlGlhJVih6ZMDEPl0Cu0syJZJvNCqXYWPA2UGfTHcZtw",
+        callbackURL: 'http://oauthall.herokuapp.com/login/twitter/return'
+    },
+    function (token, tokenSecret, profile, cb) {
+        // In this example, the user's Twitter profile is supplied as the user
+        // record.  In a production-quality application, the Twitter profile should
+        // be associated with a user record in the application's database, which
+        // allows for account linking and authentication with other identity
+        // providers.
+        return cb(null, profile);
+    }));
+
+passport.use(new facebookStrategy({
+        clientID: "866430853477178",
+        clientSecret: "20066cefc4ebcc34cccbc667c0f88480",
+        callbackURL: "http://oauthall.herokuapp.com/auth/facebook/callback",
+        enableProof: false
+    },
+    function (accessToken, refreshToken, profile, done) {
+        User.findOrCreate({facebookId: profile.id}, function (err, user) {
+            return done(err, user);
+        });
+    }
+));
 
 
 // Configure Passport authenticated session persistence.
@@ -35,12 +49,13 @@ passport.use(new Strategy({
 // from the database when deserializing.  However, due to the fact that this
 // example does not have a database, the complete Twitter profile is serialized
 // and deserialized.
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
+
+passport.serializeUser(function (user, cb) {
+    cb(null, user);
 });
 
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
+passport.deserializeUser(function (obj, cb) {
+    cb(null, obj);
 });
 
 
@@ -49,14 +64,14 @@ var app = express();
 
 // Configure view engine to render EJS templates.
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(require('body-parser').urlencoded({extended: true}));
+app.use(require('express-session')({secret: 'keyboard cat', resave: true, saveUninitialized: true}));
 
 // Initialize Passport and restore authentication state, if any, from the
 // session.
@@ -66,28 +81,43 @@ app.use(passport.session());
 
 // Define routes.
 app.get('/',
-  function(req, res) {
-    res.render('home', { user: req.user });
-  });
-
-app.get('/login',
-  function(req, res){
-    res.render('login');
-  });
-
-app.get('/login/twitter',
-  passport.authenticate('twitter'));
-
-app.get('/login/twitter/return', 
-  passport.authenticate('twitter', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
+    function (req, res) {
+        res.render('home', {user: req.user});
+    });
 
 app.get('/profile',
-  require('connect-ensure-login').ensureLoggedIn(),
-  function(req, res){
-    res.render('profile', { user: req.user });
-  });
+    require('connect-ensure-login').ensureLoggedIn(),
+    function (req, res) {
+        res.render('profile', {user: req.user});
+    });
 
-app.listen(process.env.PORT);
+app.get('/login',
+    function (req, res) {
+        res.render('login');
+    });
+
+// TWITTER METHODS
+app.get('/login/twitter',
+    passport.authenticate('twitter'));
+
+app.get('/login/twitter/return',
+    passport.authenticate('twitter', {failureRedirect: '/login'}),
+    function (req, res) {
+        res.redirect('/');
+    });
+
+// FACEBOOK METHODS
+app.get('/auth/facebook',
+    passport.authenticate('facebook'),
+    function(req, res){
+        // The request will be redirected to Facebook for authentication, so this
+        // function will not be called.
+    });
+
+app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function(req, res) {
+        res.redirect('/');
+    });
+
+app.listen(process.env.PORT || 3000);
